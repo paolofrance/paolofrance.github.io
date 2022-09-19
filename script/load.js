d577a65abb5723f2db696db02588d967ffb801a3
marie <franceschi.pl@gmail.com>
2022-09-19 13:31:43 +0200

    wip

--- script/load.js
+++ script/load.js
@@ -10,3 +10,10 @@ $(function(){
 $(function(){
     $("#social").load("social.html"); 
 });
+
+
+$(function($) {
+    $('nav li').click(function() {
+      $('section:nth-of-type('+$(this).data('rel')+')').stop().fadeIn(1, 'linear').siblings('section').stop().fadeOut(1, 'linear'); 
+    });
+  })(jQuery);
