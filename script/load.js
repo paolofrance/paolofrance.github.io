4b3d021189307e4896d83208130e0feb40a1c1f4
marie <franceschi.pl@gmail.com>
2022-09-20 03:14:24 +0200

    paolo:wip

--- script/load.js
+++ script/load.js
@@ -11,9 +11,3 @@ $(function(){
     $("#social").load("social.html"); 
 });
 
-
-$(function($) {
-    $('nav li').click(function() {
-      $('section:nth-of-type('+$(this).data('rel')+')').stop().fadeIn(1, 'linear').siblings('section').stop().fadeOut(1, 'linear'); 
-    });
-  })(jQuery);
---
+++ script/thesis.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".thesis").hide();
+});
+
+$(document).ready(function(){
+    $('section nav li:nth-of-type(1)').addClass('nav-hover'); 
+});
+  
+$(function($) {
+    $('nav li').click(function() {
+        $('section:nth-of-type('+$(this).data('rel')+')').show().siblings('section').hide(); 
+        
+
+        $('section nav li:nth-of-type('+$(this).data('rel')+')').addClass('nav-hover').siblings().removeClass('nav-hover');
+    });
+  })(jQuery);
